67815bd0047c02b93379135941e04310780f7604
Ngoc Trieu <ngoctk61@gmail.com>
2025-06-22 10:50:18 +0700

    commit

---
+++ src/Java_basic/Random.java
@@ -0,0 +1,11 @@
+package Java_basic;
+
+public class Random {
+    public static void main(String[] args){
+        java.util.Random rand= new java.util.Random();
+
+        System.out.println("peter" + new java.util.Random().nextInt(999)+"@hotmail.com");
+        System.out.println("peter" + new java.util.Random().nextInt(999)+"@hotmail.com");
+        System.out.println("peter" + new java.util.Random().nextInt(999)+"@hotmail.com");
+    }
+}
